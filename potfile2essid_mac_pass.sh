6450278d92a90d60d20438cbd1a1d72f8690e10f
logiczny <33063528+logiczny@users.noreply.github.com>
2023-01-17 23:51:38 +0100

    create script

---
+++ potfile2essid_mac_pass.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+white='\e[97m'
+purple='\e[35m'
+cyan='\e[36m'
+noColor='\e[39m'
+
+if [ $# -eq 2 ]; then
+    cat $1 | while read line 
+    do
+        hash_n_pass_cracked=$(echo $line | sed  's/^.*\*//')
+        hash_cracked=$(echo $hash_n_pass_cracked | sed 's/:.*//')
+        pass_cracked=$(echo $hash_n_pass_cracked | sed 's/^.*://')
+        dumped_line=$(grep $hash_cracked $2 | head -n1)
+        mac_of_cracked_AP=$(echo $dumped_line | sed  's/^WPA\*..\*[abcdef01234567890]*\*[abcdef01234567890]*\*//' | sed 's/\*.*//')
+
+        whoismac -p $dumped_line | sed 's/VENDOR.*//' | sed 's/MAC_STA.*//' | sed '/^[[:space:]]*$/d'
+        printf "Pass   : ${cyan}$pass_cracked${noColor}\n"
+        printf "${white}---${noColor}\n"
+    done
+else
+    printf "Usage:\n${white}potfile2essid_n_pass.sh${noColor} ${purple}hashcat.potfile${noColor} ${cyan}hash.hc22000${noColor}\n\n"
+fi
